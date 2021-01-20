15796caada660ee0909f6e0f5d9bea9206387155
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2021-01-21 04:30:36 +0600

    processing

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import logo from './logo.svg';
+import './App.css';
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+firebase.initializeApp(firebaseConfig);
+
+function App() {
+  const [user,setUser]=useState(
+    {
+     
+      isSignedIn : false,
+      name:'',
+      email:'',
+      photo:''
+
+
+    }
+  )
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+    const signInHandle= ()=> {
+      firebase.auth()
+  .signInWithPopup(provider)
+  .then(res =>{
+    const { displayName,photoURL,email}=res.user;
+    const signInUser ={
+      isSignedIn : true,
+      name : displayName,
+      photo : photoURL,
+      email : email
+    }
+    console.log(photoURL,displayName,email);
+    setUser(signInUser);
+
+    console.log(res);
+      })
+
+ .catch(err =>{
+   console.log(err);
+   console.log(err.message )
+ })
+    }
+
+ const signOutHandle=()=>{
+  firebase.auth().signOut()
+  .then(res=>{
+    const signOutUser={
+      isSignedIn : false,
+      name:'',
+      email:'',
+      photo:''
+    }
+    setUser(signOutUser)
+  })
+  .catch(err=>{
+    console.log(err);
+   console.log(err.message )
+  }
+    
+    )
+ }
+
+  return (
+    <div className="App">
+      {
+             user.isSignedIn ?  <button onClick={signOutHandle}>Sign out</button>:
+             <button onClick={signInHandle}>Sign In</button>
+
+      }
+      
+       {
+            user.isSignedIn && <div>
+                <p> Welcome, {user.name}</p>
+                <p>your email : {user.email}</p>
+                <img src={user.photo} alt=""/>
+
+            </div>
+                                  
+       }
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCPtYplBJxRqjXIU8vQ4B5wCG5SOOoP1SI",
+    authDomain: "ema-john-simple-project-edbb6.firebaseapp.com",
+    projectId: "ema-john-simple-project-edbb6",
+    storageBucket: "ema-john-simple-project-edbb6.appspot.com",
+    messagingSenderId: "587054842075",
+    appId: "1:587054842075:web:c6ecbb2040e52ffd2c4cfe",
+    measurementId: "G-YVBVM1Z30R"
+  };
+  export default firebaseConfig;
